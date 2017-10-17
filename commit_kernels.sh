186ba0bdd55d7dd6e88e4d0c9491b10989d4853b
Adam Harries <harries.adam@gmail.com>
2017-10-17 15:43:14 +0100

    add head folder to commit_kernels.sh

--- commit_kernels.sh
+++ commit_kernels.sh
@@ -25,6 +25,13 @@ mkdir -p $dname
 # copy all the kernels from $lift/scripts/generated_programs/ to $dname
 cp -r $lift/scripts/generated_programs/* $dname
 
+# copy $dname to a folder for the head, after clearing it
+hname="kernels/head"
+mkdir -p $hname
+rm -f $hname/*
+cp -r $dname/* $hname
+
 # add those files to git
 git add $dname
+git add $hname
 git commit -a -m "add kernels from lift hash: $lghash"
